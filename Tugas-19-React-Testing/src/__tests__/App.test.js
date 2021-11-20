4a1052768832c97ba8517ff2ee2dfa3d744f143f
Al-Abror <farhan.alabror@gmail.com>
2021-11-20 22:57:45 +0700

    test_

--- Tugas-19-React-Testing/src/__tests__/App.test.js
+++ Tugas-19-React-Testing/src/__tests__/App.test.js
@@ -2,17 +2,57 @@ import { AddTodo } from '../components/helper';
 
 test('Should add todo to the list', () => {
   // Arrange
+  const value1 = [{
+    id: 1,
+    name: 'Learn JSX',
+    isComplete: false,
+  }] 
+  const value2 ={
+    id: 2,
+    name: 'Build awasome react app',
+    isComplete: false,
+  }
+  const outputValue = [ 
+    {
+      id: 2,
+      name: 'Build awasome react app',
+      isComplete: false,
+    },{
+      id: 1,
+      name: 'Learn JSX',
+      isComplete: false,
+    }
+  ]
 
   // Act
+  const expected = AddTodo(value1, value2)
 
   // Assert
+  expect(expected).toEqual(outputValue)
 })
 
 test('should not mutate the existing todo array', () => {
   // Arrange
-
+  const value = [[{
+    id: 1,
+    name: 'Learn JSX',
+    isComplete: false,
+  }], {
+    id: 2,
+    name: 'Build awasome react app',
+    isComplete: false,
+  }]
+  const outputValue = [ 
+    {
+      id: 2,
+      name: 'Build awasome react app',
+      isComplete: false,
+    }
+  ]
   // Act
+  const expected = AddTodo(value[0], value[1])
 
   // Assert
+  expect(expected).not.toEqual(outputValue)
 
 })
--- Tugas-19-React-Testing/src/__tests__/Math.test.js
+++ Tugas-19-React-Testing/src/__tests__/Math.test.js
@@ -2,32 +2,39 @@ import { Add, Substract, Multiplication } from '../Math'
 
 test('Return sum of two arguments', () => {
   // Arrange
+  const value = [1, 2]
+  const outputValue = 3
 
   // Act
-
+  const expected = Add(value[0], value[1])
 
   // Assert
+  expect(expected).toBe(outputValue)
 
 })
 
 test('Return substraction of a - b', () => {
   // Arrange
-
+  const value = [2, 1]
+  const outputValue = 1
 
   // Act
-
+  const expected = Substract(value[0], value[1])
 
   // Assert
+  expect(expected).toBe(outputValue)
 
 })
 
 test('Return multiplication of two arguments', () => {
   // Arrange
-
+  const value = [3, 5]
+  const outputValue = 15
 
   // Act
-
+  const expected = Multiplication(value[0], value[1])
 
   // Assert
+  expect(expected).toBe(outputValue)
 
 })
